a0c5249a56fe5eba483d7d0032f2d87aa6b24115
Nikita1814 <kravchenko1814@gmail.com>
2021-09-04 23:06:35 +0300

    add function for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.middle = null;
+      this.range = null
+    }
+  
     setRange(min, max) {
-
+      this.range = Array.from(Array(max).keys())
     }
-
+  
     guess() {
-
+     this.middle = Math.round(this.range.length / 2)
+     return this.range[this.middle]
     }
-
+  
     lower() {
-
+      this.range = this.range.slice(0, this.middle);
     }
-
+  
     greater() {
-
+      this.range = this.range.slice(this.middle);
     }
-}
+  }
+  
 
 module.exports = GuessingGame;
